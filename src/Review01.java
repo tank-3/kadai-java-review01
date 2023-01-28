9e561cdde4f96624f12cca2a8fd78a64bcf437f0
tank-3 <taku3.na3.1415@gmail.com>
2023-01-28 18:17:08 +0900

    再提出1,taxメソッド修正

--- src/Review01.java
+++ src/Review01.java
@@ -4,11 +4,11 @@ public class Review01 {
     public static void main(String[] args) {
         int price = 1500;
         double tax = 0.1;
-        int taxPrice = getTaxPrice(price, tax);
+        int taxPrice = tax(price, tax);
         int taxIncludePrice = price + taxPrice;
         System.out.println(price + "円の商品の税込価格は" + taxIncludePrice + "円（消費税は" + taxPrice + "円）です。");
     }
-    public static int getTaxPrice(int price, double tax) {
+    public static int tax(int price, double tax) {
         int result = (int) (price * tax);
         return result;
     }
