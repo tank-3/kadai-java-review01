832c5c7d7a956112c265d5e61b158b4e27822e83
tank-3 <taku3.na3.1415@gmail.com>
2023-01-28 17:34:53 +0900

    ver1

---
+++ src/Review01.java
@@ -0,0 +1,16 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int price = 1500;
+        double tax = 0.1;
+        int taxPrice = getTaxPrice(price, tax);
+        int taxIncludePrice = price + taxPrice;
+        System.out.println(price + "円の商品の税込価格は" + taxIncludePrice + "円（消費税は" + taxPrice + "円）です。");
+    }
+    public static int getTaxPrice(int price, double tax) {
+        int result = (int) (price * tax);
+        return result;
+    }
+
+}
